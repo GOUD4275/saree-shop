9560eaa48e2ac438607866a892aaee979199ebf7
nareshgowd <gowd2009@gmail.com>
2026-03-20 22:23:42 +0530

    added files\

---
+++ cart.js
@@ -0,0 +1,34 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(name, price) {
+  cart.push({name, price});
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert("Added to cart");
+}
+
+function loadCart() {
+  let cartDiv = document.getElementById("cart-items");
+  let total = 0;
+
+  cartDiv.innerHTML = "";
+
+  cart.forEach(item => {
+    cartDiv.innerHTML += `<p>${item.name} - ₹${item.price}</p>`;
+    total += item.price;
+  });
+
+  document.getElementById("total").innerText = "Total: ₹" + total;
+}
+
+function checkout() {
+  let message = "Order Details:\n";
+  cart.forEach(item => {
+    message += item.name + " - ₹" + item.price + "\n";
+  });
+
+  window.open(`https://wa.me/919959008593?text=${encodeURIComponent(message)}`);
+}
+
+if (document.getElementById("cart-items")) {
+  loadCart();
+}
